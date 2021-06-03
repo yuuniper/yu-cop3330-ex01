b1a3d1a3c9a4988afdc0e2ba365fc944d8113852
Alice Yu <alicesyu2002@gmail.com>
2021-06-03 04:59:52 -0400

    Exercise 1

---
+++ src/main/java/ex01/App.java
@@ -0,0 +1,21 @@
+package ex01;
+
+/*
+Exercise 1 - Saying Hello
+Create a program that prompts for your name and prints a greeting using your name.
+
+Example Output
+What is your name? Brian
+Hello, Brian, nice to meet you!
+
+Constraint
+Keep the input, string concatenation, and output separate.
+
+Challenges
+Write a new version of the program without using any variables.
+Write a version of the program that displays different greetings for different people.
+Write a version of the program that displays the greeting in a GUI window.
+ */
+
+public class App {
+}
